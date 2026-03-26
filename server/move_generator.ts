290c809b7ca706125986ccea5f6469869422111a
Sfabi28 <samuelefabi28@gmail.com>
2026-03-26 14:02:49 +0100

    moves for rook, pawn, bishop, queen

--- server/move_generator.ts
+++ server/move_generator.ts
@@ -1,9 +1,10 @@
+import { moveMessagePortToContext } from 'node:worker_threads';
 import type { GameState, Square, Board, Piece, Color } from '../shared/types'
 
 function generateMovesForPiece(square: Square, piece: Piece, board: Board): Square[] {
     switch (piece.figure) {
         case 'p':
-            return generatePawnMoves(square, piece.color, board);
+            return generatePawnMoves(square, piece.color, board); // TODO aggiungere promozione ed en passant
         case 'n':
             return generateKnightMoves(square, piece.color, board);
         case 'b':
@@ -17,6 +18,146 @@ function generateMovesForPiece(square: Square, piece: Piece, board: Board): Squa
         default:
             return [];
     }
+}
+
+function generateKingMoves(square: Square, color: Color, board: Board) : Square[] {
+
+    const moves = [[-1,1], [0,1], [1,1], [-1,0], [1,0], [-1,-1], [0,-1], [1,-1]]
+
+    const possibleMoves: Square[] = []
+
+    const currentX = square[0].charCodeAt(0) - 97
+    const currentY = parseInt(square[1]) - 1  
+
+
+
+
+    return possibleMoves
+}
+
+function generateQueenMoves(square: Square, color: Color, board: Board) : Square[] {
+    const possibleMoves: Square[] = []
+
+    const rook = generateRookMoves(square, color, board)
+    const bishop = generateBishopMoves(square, color, board)
+
+    possibleMoves.push(...rook)
+    possibleMoves.push(...bishop)
+
+    return possibleMoves
+}
+
+function generateBishopMoves(square: Square, color: Color, board: Board) : Square[] {
+    const directions = [[1, 1], [-1, -1], [-1, 1], [1, -1]]
+
+    const currentX = square[0].charCodeAt(0) - 97
+    const currentY = parseInt(square[1]) - 1
+
+    const possibleMoves: Square[] = []
+
+    for (const [dx, dy] of directions) {
+        let x = currentX + dx
+        let y = currentY + dy
+
+        while (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+            const coordX = String.fromCharCode(x + 97)
+            const coordY = (y + 1).toString()
+            const targetSquare = coordX + coordY as Square
+
+            if (!board[targetSquare]) {
+                possibleMoves.push(targetSquare)
+            } else if (board[targetSquare].color !== color) {
+                possibleMoves.push(targetSquare)
+                break
+            } else {
+                break
+            }
+
+            x += dx
+            y += dy
+        }
+    }
+
+    return possibleMoves   
+}
+
+function generateRookMoves(square: Square, color: Color, board: Board): Square[] {
+    const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+    const currentX = square[0].charCodeAt(0) - 97
+    const currentY = parseInt(square[1]) - 1
+
+    const possibleMoves: Square[] = []
+
+    for (const [dx, dy] of directions) {
+        let x = currentX + dx
+        let y = currentY + dy
+
+        while (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+            const coordX = String.fromCharCode(x + 97)
+            const coordY = (y + 1).toString()
+            const targetSquare = coordX + coordY as Square
+
+            if (!board[targetSquare]) {
+                possibleMoves.push(targetSquare)
+            } else if (board[targetSquare].color !== color) {
+                possibleMoves.push(targetSquare)
+                break
+            } else {
+                break
+            }
+
+            x += dx
+            y += dy
+        }
+    }
+
+    return possibleMoves
+}
+
+function generatePawnMoves(square: Square, color: Color, board: Board): Square[] {
+
+    const currentX = square[0].charCodeAt(0) - 97
+    const currentY = parseInt(square[1]) - 1
+    const possibleMoves: Square[] = []
+
+    const direction = color === 'w' ? 1 : -1
+    const startRank = color === 'w' ? 1 : 6
+
+    const toSquare = (x: number, y: number): Square =>
+        (String.fromCharCode(x + 97) + (y + 1).toString()) as Square
+
+    const oneStepY = currentY + direction
+    if (oneStepY >= 0 && oneStepY <= 7) { //spostamento e spostamento iniziale
+        const oneStepSquare : Square = toSquare(currentX, oneStepY)
+        if (!board[oneStepSquare]) {
+            possibleMoves.push(oneStepSquare)
+
+            const twoStepY = currentY + 2 * direction
+            if (currentY === startRank && twoStepY >= 0 && twoStepY <= 7) {
+                const twoStepSquare = toSquare(currentX, twoStepY)
+                if (!board[twoStepSquare]) {
+                    possibleMoves.push(twoStepSquare)
+                }
+            }
+        }
+    }
+
+    for (const dx of [-1, 1]) { //mangiare
+        const targetX = currentX + dx
+        const targetY = currentY + direction
+
+        if (targetX < 0 || targetX > 7 || targetY < 0 || targetY > 7) continue
+
+        const targetSquare = toSquare(targetX, targetY)
+        const targetPiece = board[targetSquare]
+
+        if (targetPiece && targetPiece.color !== color) {
+            possibleMoves.push(targetSquare)
+        }
+    }
+
+    return possibleMoves
 }
 
 function generateKnightMoves(square: Square, color: Color, board: Board): Square[] {
